7272540d4c0c5159d081554291ccf0b25127e4fc
Santiago Garcia <santiago.garcia@posteo.de>
2025-08-09 13:54:44 +0200

    Update logos and names

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,44 +1,44 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import tailwind from '@astrojs/tailwind';
-import compress from 'astro-compress';
-import sitemap from '@astrojs/sitemap';
+import tailwind from "@astrojs/tailwind";
+import compress from "astro-compress";
+import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
-    output: 'static',
-    trailingSlash: 'always',
-    site: 'https://devidev.io',
+  output: "static",
+  trailingSlash: "always",
+  site: "https://santiagogarcia.de",
 
-    // Single page, no prefetch needed
-    prefetch: false,
+  // Single page, no prefetch needed
+  prefetch: false,
 
-    integrations: [
-        tailwind(),
-        sitemap(),
-        compress({
-            CSS: true,
-            SVG: false,
-            Image: false,
-            HTML: {
-                "html-minifier-terser": {
-                    collapseWhitespace: true,
-                    // collapseInlineTagWhitespace: true, // It breaks display-inline / flex-inline text
-                    minifyCSS: true,
-                    minifyJS: true,
-                    removeComments: true,
-                    removeEmptyAttributes: true,
-                    // removeEmptyElements: true, // It removes sometimes SVGs
-                    removeRedundantAttributes: true
-                },
-            },
-            JavaScript: {
-                'terser': {
-                    compress: {
-                        drop_console: true,
-                        drop_debugger: true,
-                    }
-                }
-            }
-        })
-    ]
+  integrations: [
+    tailwind(),
+    sitemap(),
+    compress({
+      CSS: true,
+      SVG: false,
+      Image: false,
+      HTML: {
+        "html-minifier-terser": {
+          collapseWhitespace: true,
+          // collapseInlineTagWhitespace: true, // It breaks display-inline / flex-inline text
+          minifyCSS: true,
+          minifyJS: true,
+          removeComments: true,
+          removeEmptyAttributes: true,
+          // removeEmptyElements: true, // It removes sometimes SVGs
+          removeRedundantAttributes: true,
+        },
+      },
+      JavaScript: {
+        terser: {
+          compress: {
+            drop_console: true,
+            drop_debugger: true,
+          },
+        },
+      },
+    }),
+  ],
 });
